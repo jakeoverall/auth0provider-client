a05f0f3f7d6d4b901c8f017821e4e51e72f3777b
Jake Overall <joverall22@gmail.com>
2020-08-13 19:44:22 -0600

    release 0.0.1

---
+++ examples/app.js
@@ -0,0 +1,26 @@
+import { Auth0Provider } from '../lib'
+
+const domain = ""
+const clientId = ""
+const audience = ""
+
+export const AuthService = Auth0Provider.initialize({
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: appState => {
+    window.location.replace(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    )
+  }
+})
+
+// Listen for secific AUTH_EVENTS hooks
+AuthService.on(AuthService.AUTH_EVENTS.AUTHENTICATED, async () => {
+  // AuthService.user is now defined
+  const user = AuthService.user
+  //Set bearer token on all requests
+  // example: $resource.defaultHeaders.Authorization = AuthService.bearer
+})

9d0dddb12efb16349ed463f6451a61e334f6726c
Jake Overall <joverall22@gmail.com>
2022-01-12 02:42:31 -0700

    Create index.d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,113 @@
+export interface AuthServiceMethodOptions {
+  display?: 'page' | 'popup' | 'touch' | 'wap'
+  prompt?: 'none' | 'login' | 'consent' | 'select_account'
+  max_age?: string | number
+  ui_locales?: string
+  id_token_hint?: string
+  login_hint?: string
+  acr_values?: string
+  scope?: string
+  audience?: string
+  connection?: string
+  [key: string]: any
+}
+
+export class EventEmitter {
+  _listeners: { [key: string]: any[] };
+  on(event: string | number, fn: function, thisContext: any = null): void
+  off(event: string | number, fn: function): void
+  emit(event: string, payload: any): void
+}
+
+export type AUTH_EVENTS = {
+  LOADING: 'LOADING',
+  LOADED: 'LOADED',
+  AUTHENTICATED: 'AUTHENTICATED',
+  TOKEN_CHANGE: 'TOKEN_CHANGE'
+}
+
+export class AuthPlugin extends EventEmitter {
+  AUTH_EVENTS: AUTH_EVENTS
+  options: Auth0ConfigurationOptions
+  loading: boolean
+  isAuthenticated: boolean
+  user: any
+  userInfo: any
+  identity: any
+  bearer: string
+  auth0Client: any
+  popupOpen: boolean
+  error?: Error
+  created(options): AuthPlugin
+
+
+  /** Authenticates the user using a popup window */
+  async loginWithPopup(options: AuthServiceMethodOptions): Promise<void>
+
+  /** Handles the callback when logging in using a redirect */
+  async handleRedirectCallback(): Promise<void>
+
+  /** Authenticates the user using the redirect method */
+  loginWithRedirect(o: AuthServiceMethodOptions): Promise<void>
+
+  /**
+   * Returns all the claims present in the ID token
+   */
+  getIdTokenClaims(o: AuthServiceMethodOptions): any
+
+  /**
+   * Returns the access token. If the token is invalid or missing, a new one is retrieved
+   */
+  async getTokenSilently(o: AuthServiceMethodOptions): Promise<string>
+
+  /**
+   * @param {string[] | string} permissions
+   */
+  hasPermissions(permissions: string[] | string): Promise<boolean>
+
+  /**
+   * @param {string[] | string} roles
+   */
+  hasRoles(roles: string[] | string): Promise<boolean>
+
+
+  async getIdentityClaims(token: string): Promise<any>
+
+  /**
+   * Gets the access token using a popup window
+   */
+  async getTokenWithPopup(o: AuthServiceMethodOptions): Promise<string>
+
+  async getUserData(): Promise<void>
+
+  /** Logs the user out and removes their session on the authorization server */
+  logout(o: AuthServiceMethodOptions): void
+
+  /**
+   * Use this lifecycle method to instantiate the SDK client
+   */
+  async created(options: AuthServiceMethodOptions): Promise<void>
+}
+
+
+export interface Auth0ConfigurationOptions {
+  onRedirectCallback: () => void
+  domain: string
+  audience: string
+  clientId: string
+  useRefreshTokens?: boolean
+}
+
+export function onAuthLoaded(cb: () => any): Promise<AuthPlugin>
+export function b64DecodeUnicode(str: string = '.'): string
+export function decodeToken(str: string = '.'): string
+
+export function initialize(options: Auth0ConfigurationOptions): AuthPlugin
+
+export async function authGuard(to: any, from: any, next: () => any): Promise<any>
+
+export async function authSettled(to: any, from: any, next: () => any): Promise<any>
+
+export const hasPermissions = (permissions: string | string[]) => instance.hasPermissions(permissions)
+
+export const hasRoles = (roles: string | string[]) => instance.hasRoles(roles)

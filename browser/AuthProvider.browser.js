d20977ca7654985ec5a509af0b3ec24976eb13b5
Jake Overall <joverall22@gmail.com>
2022-08-13 03:16:54 -0600

    Update AuthProvider.browser.js

--- browser/AuthProvider.browser.js
+++ browser/AuthProvider.browser.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   class EventEmitter {
     constructor() {
       this._listeners = {}
@@ -48,69 +48,64 @@
   const DEFAULT_REDIRECT_CALLBACK = () =>
     window.history.replaceState({}, document.title, window.location.pathname)
 
-  /**
- @type {AuthPlugin}
- */
-  let instance
+
+  let instance = {}
 
   class AuthPlugin extends EventEmitter {
     constructor(options = {}) {
-      if (instance) { return instance }
       super()
       instance = this
-      this.AUTH_EVENTS = {
+      instance.loading = true
+      instance.AUTH_EVENTS = {
         LOADING: 'LOADING',
         LOADED: 'LOADED',
         AUTHENTICATED: 'AUTHENTICATED',
         TOKEN_CHANGE: 'TOKEN_CHANGE'
       }
-      this.options = options
-      this.options.onRedirectCallback = this.options.onRedirectCallback || DEFAULT_REDIRECT_CALLBACK
-      this.loading = true
-      this.isAuthenticated = false
-      this.user = {}
-      this.userInfo = {}
-      this.identity = {}
-      this.bearer = ''
-      this.auth0Client = null
-      this.popupOpen = false
-      this.error = null
-      this.created(options)
-      return instance
+      instance.user = {}
+      instance.userInfo = {}
+      instance.identity = {}
+      instance.isAuthenticated = false
+      instance.bearer = ''
+      instance.options = options
+      instance.popupOpen = false
+      instance.auth0Client = null
+      instance.error = null
+      instance.options.onRedirectCallback = instance.options.onRedirectCallback || DEFAULT_REDIRECT_CALLBACK
+      instance.created(options)
+      instance.created = true
     }
 
     /** Authenticates the user using a popup window */
     async loginWithPopup(o = {
       returnTo: window.location.origin
     }) {
-      this.popupOpen = true
-
+      instance.popupOpen = true
       try {
-        await this.auth0Client.loginWithPopup(o)
-        this.user = await this.auth0Client.getUser()
-        this.user = this.user || {}
-        await this.getUserData()
-        this.isAuthenticated = true
+        await instance.auth0Client.loginWithPopup(o)
+        instance.user = await instance.auth0Client.getUser()
+        instance.user = instance.user || {}
+        await instance.getUserData()
+        instance.isAuthenticated = true
       } catch (e) {
-      // eslint-disable-next-line
-      console.error(e);
+        instance.loginWithRedirect()
       } finally {
-        this.popupOpen = false
+        instance.popupOpen = false
       }
     }
 
     /** Handles the callback when logging in using a redirect */
     async handleRedirectCallback() {
-      this.loading = true
+      instance.loading = true
       try {
-        await this.auth0Client.handleRedirectCallback()
-        this.user = await this.auth0Client.getUser()
-        await this.getUserData()
-        this.isAuthenticated = true
+        await instance.auth0Client.handleRedirectCallback()
+        instance.user = await instance.auth0Client.getUser()
+        await instance.getUserData()
+        instance.isAuthenticated = true
       } catch (e) {
-        this.error = e
+        instance.error = e
       } finally {
-        this.loading = false
+        instance.loading = false
       }
     }
 
@@ -119,7 +114,7 @@
       if (!o.appState) {
         o.redirectUri = o.redirectUri || window.location.href
       }
-      return this.auth0Client.loginWithRedirect(o)
+      return instance.auth0Client.loginWithRedirect(o)
     }
 
     /**
@@ -127,7 +122,7 @@
    * @param {import("@auth0/auth0-spa-js").GetIdTokenClaimsOptions} o
    */
     getIdTokenClaims(o) {
-      return this.auth0Client.getIdTokenClaims(o)
+      return instance.auth0Client.getIdTokenClaims(o)
     }
 
     /**
@@ -135,9 +130,9 @@
    * @param {import("../lib").AuthServiceMethodOptions} [o]
    */
     async getTokenSilently(o) {
-      const token = await this.auth0Client.getTokenSilently(o)
-      this.getIdentityClaims(token)
-      this.emit(this.AUTH_EVENTS.TOKEN_CHANGE, this)
+      const token = await instance.auth0Client.getTokenSilently(o)
+      instance.getIdentityClaims(token)
+      instance.emit(instance.AUTH_EVENTS.TOKEN_CHANGE, this)
       return token
     }
 
@@ -148,7 +143,7 @@
       if (!Array.isArray(permissions)) {
         permissions = [permissions]
       }
-      if (!this.identity.permissions) {
+      if (!instance.identity.permissions) {
         return false
       }
       while (permissions.length) {
@@ -156,7 +151,7 @@
         const /**
          * @param {any} p
          */
-          found = this.identity.permissions.find(p => p === next)
+          found = instance.identity.permissions.find(p => p === next)
         if (!found) {
           return false
         }
@@ -171,7 +166,7 @@
       if (!Array.isArray(roles)) {
         roles = [roles]
       }
-      if (!this.userInfo.roles) {
+      if (!instance.userInfo.roles) {
         return false
       }
       while (roles.length) {
@@ -179,7 +174,7 @@
         const /**
          * @param {any} r
          */
-          found = this.userInfo.roles.find(r => r === next)
+          found = instance.userInfo.roles.find(r => r === next)
         if (!found) {
           return false
         }
@@ -191,8 +186,8 @@
    * @param {string} token
    */
     async getIdentityClaims(token) {
-      this.identity = JSON.parse(decodeToken(token))
-      return this.identity
+      instance.identity = JSON.parse(decodeToken(token))
+      return instance.identity
     }
 
     /**
@@ -200,21 +195,21 @@
    * @param {import("../lib").AuthServiceMethodOptions} o
    */
     async getTokenWithPopup(o) {
-      const token = await this.auth0Client.getTokenWithPopup(o)
-      this.getIdentityClaims(token)
-      this.emit(this.AUTH_EVENTS.TOKEN_CHANGE, this)
+      const token = await instance.auth0Client.getTokenWithPopup(o)
+      instance.getIdentityClaims(token)
+      instance.emit(instance.AUTH_EVENTS.TOKEN_CHANGE, this)
       return token
     }
 
     async getUserData() {
       try {
-        const token = await this.getTokenSilently()
-        await this.getIdentityClaims(token)
-        this.bearer = 'Bearer ' + token
+        const token = await instance.getTokenSilently()
+        await instance.getIdentityClaims(token)
+        instance.bearer = 'Bearer ' + token
         // eslint-disable-next-line no-undef
-        const res = await fetch(`https://${this.options.domain}/userinfo`, {
+        const res = await fetch(`https://${instance.options.domain}/userinfo`, {
           headers: {
-            authorization: this.bearer
+            authorization: instance.bearer
           }
         })
 
@@ -224,11 +219,11 @@
           if (key.includes('https')) {
             keep = keep.slice(keep.lastIndexOf('/') + 1)
           }
-          this.userInfo[keep] = userData[key]
+          instance.userInfo[keep] = userData[key]
         }
-        this.user = this.user || {}
-        this.user.isAuthenticated = true
-        this.emit(this.AUTH_EVENTS.AUTHENTICATED, this)
+        instance.user = instance.user || {}
+        instance.user.isAuthenticated = true
+        instance.emit(instance.AUTH_EVENTS.AUTHENTICATED, this)
       } catch (e) {
         // eslint-disable-next-line no-console
         console.error(e)
@@ -239,12 +234,12 @@
     logout(o = {
       returnTo: window.location.origin
     }) {
-      const logout = this.auth0Client.logout(o)
-      this.bearer = ''
-      this.user = {}
-      this.userInfo = {}
-      this.identity = {}
-      this.isAuthenticated = false
+      const logout = instance.auth0Client.logout(o)
+      instance.bearer = ''
+      instance.user = {}
+      instance.userInfo = {}
+      instance.identity = {}
+      instance.isAuthenticated = false
       return logout
     }
 
@@ -253,11 +248,11 @@
    * @param {{ domain?: any; clientId?: any; audience?: any; redirectUri?: any; onRedirectCallback?: any; useRefreshTokens?: boolean }} options
    */
     async created(options) {
-      this.emit(this.AUTH_EVENTS.LOADING)
+      instance.emit(instance.AUTH_EVENTS.LOADING)
       // Create a new instance of the SDK client using members of the given options object
       // @ts-ignore
       // eslint-disable-next-line no-undef
-      this.auth0Client = await createAuth0Client({
+      instance.auth0Client = await createAuth0Client({
         domain: options.domain,
         client_id: options.clientId,
         audience: options.audience,
@@ -266,27 +261,34 @@
       })
 
       try {
-      // If the user is returning to the app after authentication..
+        // If the user is returning to the app after authentication..
         if (
           window.location.search.includes('code=') &&
-        window.location.search.includes('state=')
+          window.location.search.includes('state=')
         ) {
-        // handle the redirect and retrieve tokens
-          const { appState } = await this.auth0Client.handleRedirectCallback()
+          // handle the redirect and retrieve tokens
+          const { appState } = await instance.auth0Client.handleRedirectCallback()
 
           // Notify subscribers that the redirect callback has happened, passing the appState
           // (useful for retrieving any pre-authentication state)
           options.onRedirectCallback(appState)
         }
       } catch (e) {
-        this.error = e
+        instance.error = e
       } finally {
-      // Initialize our internal authentication state
-        this.isAuthenticated = await this.auth0Client.isAuthenticated()
-        this.user = await this.auth0Client.getUser()
-        await this.getUserData()
-        this.loading = false
-        this.emit(this.AUTH_EVENTS.LOADED)
+        // Initialize our internal authentication state
+        instance.isAuthenticated = await instance.auth0Client.isAuthenticated()
+
+        if (!instance.isAuthenticated) {
+          instance.loading = false
+          instance.emit(instance.AUTH_EVENTS.LOADED)
+          return
+        }
+
+        instance.user = await instance.auth0Client.getUser()
+        await instance.getUserData()
+        instance.loading = false
+        instance.emit(instance.AUTH_EVENTS.LOADED)
       }
     }
   }
@@ -309,7 +311,7 @@
     try {
       return decodeURIComponent(
         // eslint-disable-next-line no-undef
-        atob(str).replace(/(.)/g, function(m, p) {
+        atob(str).replace(/(.)/g, function (m, p) {
           let code = p
             .charCodeAt(0)
             .toString(16)
@@ -349,10 +351,13 @@
   }
 
   const Auth0Provider = {
-  /**
- * @param {{ onRedirectCallback: () => void; domain: string, audience: string, clientId: string  }} options
- */
-    initialize(options) { return new AuthPlugin(options) },
+    /**
+   * @param {{ onRedirectCallback: () => void; domain: string, audience: string, clientId: string  }} options
+   */
+    initialize(options) {
+      if (instance && instance.created) { return instance }
+      return new AuthPlugin(options)
+    },
     /**
   * @param {{ fullPath: any; }} to
   * @param {any} from
@@ -376,19 +381,19 @@
   // @ts-ignore
   window.Auth0Provider = Auth0Provider
 
-/**
- * @typedef {{
-  * display?: 'page' | 'popup' | 'touch' | 'wap',
-  * prompt?: 'none' | 'login' | 'consent' | 'select_account',
-  * max_age?: string | number  ,
-  * ui_locales?: string,
-  * id_token_hint?: string,
-  * login_hint?: string,
-  * acr_values?: string,
-  * scope?: string,
-  * audience?: string,
-  * connection?: string,
-  * [key: string]: any
-  * }} AuthServiceMethodOptions
-  */
+  /**
+   * @typedef {{
+    * display?: 'page' | 'popup' | 'touch' | 'wap',
+    * prompt?: 'none' | 'login' | 'consent' | 'select_account',
+    * max_age?: string | number  ,
+    * ui_locales?: string,
+    * id_token_hint?: string,
+    * login_hint?: string,
+    * acr_values?: string,
+    * scope?: string,
+    * audience?: string,
+    * connection?: string,
+    * [key: string]: any
+    * }} AuthServiceMethodOptions
+    */
 }())
